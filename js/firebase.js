7f742de3e9d649033c734d1eb906527cc88adc5d
kliuz <33242952+kliuz@users.noreply.github.com>
2021-02-28 00:56:17 -0500

    stub out firebase helper functions

--- js/firebase.js
+++ js/firebase.js
@@ -2,3 +2,31 @@
 firebase.initializeApp(firebaseConfig);
 
 console.log(firebase);
+
+function addUser(username) {
+  // return true if successful, false otherwise
+}
+
+function addFriend(username, friendUsername) {
+  // return true if successful, false otherwise
+}
+
+function addNote(username, url, note, public) {
+  // return true if successful, false otherwise
+}
+
+function getUser(username) {
+  // return document for this user
+}
+
+function getAllUserFriends(username) {
+  // return array of usernames
+}
+
+function getPublicNotes() {
+  // return all public notes
+}
+
+function getUserNOtes(username, public) {
+  // return notes for this user
+}
